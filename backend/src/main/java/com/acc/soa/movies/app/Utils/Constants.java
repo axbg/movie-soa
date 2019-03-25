fede46e57e17e73a3aac8c7a7d400efd24331e26
Alex Bisag <bisagalexstefan@gmail.com>
2019-03-25 13:30:18 +0200

    Front-end init.

--- backend/src/main/java/com/acc/soa/movies/app/Utils/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.acc.soa.movies.app.Utils;
-
-public interface Constants {
-    String TMDB_API = "https://api.themoviedb.org/3";
-    String TMDB_KEY = "?api_key=b250ddaa5100e19523ef694ec9a44880";
-    String PHOTO_URL = "https://image.tmdb.org/t/p/w600_and_h900_bestv2";
-}
