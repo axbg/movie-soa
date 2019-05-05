f3a12a3541d4112cebef4a12a0b3ecbdf56b9b12
Alex Bisag <bisagalexstefan@gmail.com>
2019-05-05 16:24:22 +0300

    Finished Search Functionality.

--- frontend/src/app/tab2/tab2.page.ts
+++ frontend/src/app/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpManagerService } from '../http-manager.service';
 import { ISoapMethodResponse } from 'ngx-soap';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -14,12 +15,12 @@ export class Tab2Page implements OnInit {
   private loaded: Boolean = false;
   private loadInterval;
 
-  constructor(private httpManager: HttpManagerService) {
+  constructor(private httpManager: HttpManagerService, private toastController: ToastController) {
   }
 
   ngOnInit() {
     this.loadInterval = setInterval(() => {
-      if(this.checkLoaded){
+      if (this.checkLoaded) {
         clearInterval(this.loadInterval);
         this.httpManager.addAuthHeader();
         this.getPopularMovies();
@@ -33,22 +34,53 @@ export class Tab2Page implements OnInit {
 
   shortenMoviesTagline() {
     this.movies = this.movies.map(movie => {
-      movie["tagline"] = movie["tagline"].length > 100 ? movie["tagline"].slice(0,100) + "..." : movie["tagline"];
+      movie["tagline"] = (movie["tagline"] && movie["tagline"].length > 100) ? movie["tagline"].slice(0, 100) + "..." : movie["tagline"];
       return movie;
     });
+  }
+
+  populateMovieList(movies) {
+    if (movies) {
+      this.movies = movies;
+      this.shortenMoviesTagline();
+      this.loaded = true;
+    }
   }
 
   getPopularMovies() {
     (<any>this.httpManager.client).getPopularMovie({}).subscribe(
       (res: ISoapMethodResponse) => {
-        this.movies = res.result.movies.movie;
-        this.shortenMoviesTagline();
-        this.loaded = true;
+        this.populateMovieList(res.result.movies.movie);
+        this.searchText = "";
       }
     )
   }
 
   dispatchSearch() {
-    console.log(this.searchText);
+    setTimeout(() => {
+      const title = this.searchText.replace(/ /g, '%20');
+
+      const body = {
+        "sch:title": title
+      };
+
+      this.loaded = false;
+      (<any>this.httpManager.client).getMoviesByName(body).subscribe(
+        async (res: ISoapMethodResponse) => {
+          if (res.result.movies) {
+            await this.populateMovieList(res.result.movies.movie);
+          } else {
+            const loader = await this.toastController.create({
+              message: 'Nothing found',
+              duration: 2000,
+              position: "top"
+            });
+
+            loader.present();
+          }
+          this.loaded = true;
+        }
+      )
+    }, 200);
   }
 }
