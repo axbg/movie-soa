efcf8f5261692d35badf70d76ea70bc0f25936b1
Alex Bisag <bisagalexstefan@gmail.com>
2019-05-05 15:39:46 +0300

    Finished Detailed Movie, Recommendations, Adding Movie To Collection.

--- frontend/src/app/movie-detail/movie-detail.page.ts
+++ frontend/src/app/movie-detail/movie-detail.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpManagerService } from '../http-manager.service';
+import { ISoapMethodResponse } from 'ngx-soap';
 
 @Component({
   selector: 'app-movie-detail',
@@ -8,89 +10,96 @@ import { Router } from '@angular/router';
 })
 export class MovieDetailPage implements OnInit {
 
-  //retrieved from back-end
-  movie: any = {};
+  movieLoaded: boolean = false;
+  recommendationsLoaded: boolean = false;
+  loaded: boolean = false;
+  added: boolean;
+  movie: Object = {};
+  currentId: string;
   recommended: Object[];
   slideOpts = {
     slidesPerView: 2.6,
     spaceBetween: 15
   }
 
-  constructor(private router: Router) {
-    this.movie = {
-      "name": "Avengers",
-      "photo": "https://upload.wikimedia.org/wikipedia/en/8/85/Captain_Marvel_poster.jpg",
-      "overview": "Carol Danvers becomes one of the universe's most powerful heroes when Earth is caught in the middle of a galactic war between two alien races.",
-      "genres": [
-        "Action",
-        "Drama",
-        "Superheroes"
-      ],
-      "rating": 3,
-      "id": 5,
-      "year": 2019,
-      "added": true
-    }
-
-    this.recommended = [
-      {
-        "name": "Avengers",
-        "photo": "https://upload.wikimedia.org/wikipedia/en/8/85/Captain_Marvel_poster.jpg",
-        "overview": "overview",
-        "rating": 3,
-        "id": 5
-      },
-      {
-        "name": "Captain Marvel",
-        "photo": "https://upload.wikimedia.org/wikipedia/en/8/85/Captain_Marvel_poster.jpg",
-        "overview": "overview",
-        "rating": 5,
-        "id": 12
-      },
-      {
-        "name": "Captain Marvel",
-        "photo": "https://upload.wikimedia.org/wikipedia/en/8/85/Captain_Marvel_poster.jpg",
-        "overview": "overview",
-        "rating": 5,
-        "id": 12
-      },
-      {
-        "name": "Avengers",
-        "photo": "https://upload.wikimedia.org/wikipedia/en/8/85/Captain_Marvel_poster.jpg",
-        "overview": "overview",
-        "rating": 3,
-        "id": 5
-      },
-      {
-        "name": "Captain Marvel",
-        "photo": "https://upload.wikimedia.org/wikipedia/en/8/85/Captain_Marvel_poster.jpg",
-        "overview": "overview",
-        "rating": 5,
-        "id": 12
-      },
-      {
-        "name": "Captain Marvel",
-        "photo": "https://upload.wikimedia.org/wikipedia/en/8/85/Captain_Marvel_poster.jpg",
-        "overview": "overview",
-        "rating": 5,
-        "id": 12
-      }
-    ]
+  constructor(private router: Router, private httpManager: HttpManagerService) {
+    this.currentId = this.router.url.split("/")[2];
   }
 
   ngOnInit() {
+    this.getMovieDetail();
+    this.getRecommended();
+  }
+
+  modifyLoaded() {
+    if(this.movieLoaded && this.recommendationsLoaded){
+      this.loaded = true;
+    }
+  }
+  
+  getMovieDetail() {
+    const body = {
+      "sch:movie_id": this.currentId
+    };
+
+    (<any>this.httpManager.client).getMovieById(body).subscribe(
+      (res: ISoapMethodResponse) => {
+        this.movie = {
+          title: res.result.movie.title,
+          rating: res.result.movie.rating,
+          year: res.result.movie.year,
+          tagline: res.result.movie.tagline,
+          photo: res.result.movie.photo,
+          id: res.result.movie.movie_id,
+          genres: res.result.movie.genres.genre
+        }
+
+        this.added = res.result.added;
+        this.movieLoaded = true;
+        this.modifyLoaded();
+      }
+    )
+  }
+
+  getRecommended() {
+    const body = {
+      "sch:movie_id": this.currentId
+    };
+
+    (<any>this.httpManager.client).getRecommendations(body).subscribe(
+      (res: ISoapMethodResponse) => {
+        this.recommended = res.result.movies.movie.map(movie => {
+          return {
+            id: movie.movie_id,
+            photo: movie.photo
+          }
+        })
+
+        this.recommendationsLoaded = true;
+        this.modifyLoaded();
+      }
+    )
   }
 
   isAdded() {
-    return this.movie.added ? "-" : "+";
+    return this.added ? "-" : "+";
   }
 
   changeCollectionStatus() {
-    this.movie.added = !this.movie.added;
+    const body = {
+      "sch:movie_id": this.currentId
+    };
+
+    (<any>this.httpManager.client).saveMovieToCollection(body).subscribe(
+      (res: ISoapMethodResponse) => {
+        this.added = res.result.added;
+      }
+    )
   }
 
   navigateBack() {
     window.history.back();
+    this.movie = {};
   }
 
   openRecommendation(element) {
